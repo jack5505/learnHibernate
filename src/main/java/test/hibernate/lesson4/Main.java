ba6da59fd5f83ee45c89ab83f73707e813b91d80
Jack5505 <jahon.080879@gmail.com>
2022-08-31 16:20:17 +0500

    Start the third lesson about @Enumerated with difference EnumType.String and Ordered

--- src/main/java/test/hibernate/lesson4/Main.java
+++ src/main/java/test/hibernate/lesson4/Main.java
@@ -3,6 +3,8 @@ package test.hibernate.lesson4;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import test.hibernate.lesson4.entity.Address;
+import test.hibernate.lesson4.entity.Companyies;
 
 /*
  *
@@ -13,6 +15,18 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
         EntityManager em = emf.createEntityManager();
+
+        Companyies company = new Companyies();
+        company.setName("Jakhongir Testlash");
+        Address address = new Address();
+        address.setCity("Urgench");
+        address.setNo("12");
+        address.setStreet("M.Xorazim");
+        company.setAddress(address);
+
+        em.getTransaction().begin();
+        em.persist(company);
+        em.getTransaction().commit();
 
     }
 }
--- src/main/java/test/hibernate/lesson4/entity/Companyies.java
+++ src/main/java/test/hibernate/lesson4/entity/Companyies.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /*
  *
@@ -14,7 +15,8 @@ import javax.persistence.Id;
  *
  */
 @Entity
-public class Company {
+@Table(name = "companies")
+public class Companyies {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
